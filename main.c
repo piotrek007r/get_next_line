0c4f9b39f4b354ff2f884bd7d962575caf196f70
Piotr Ruszkiewicz <pruszkie@c3r13s3.42warsaw.pl>
2024-03-15 18:29:01 +0100

    start

--- main.c
+++ main.c
@@ -1,4 +1,18 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
 int main()
 {
-	open()
+	int fd;
+	size_t bytes_read;
+	size_t count;
+	char buffer[100]; // to change
+
+	count = 5; // temporary var declariation
+	fd = open("test.txt", O_RDONLY);
+
+	bytes_read = read(fd, buffer, count);
+
+	printf("%s", buffer);
 }
